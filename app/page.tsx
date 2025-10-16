11c580f9a304342aecb67ef32c42f4a2dd1d81b4
Boris Ciobirca <boris.chobyrka@gmail.com>
2025-10-16 21:59:07 +0100

    chore: redirect to about from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  return redirect("/about");
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-14 gap-16 sm:p-20">
       <main className="flex gap-[32px] row-start-2 items-center sm:items-start">
